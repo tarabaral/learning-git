090b87580421411eaf1d74de241a5256144ada3f
tarabaral <73013994+tarabaral@users.noreply.github.com>
2020-10-24 23:26:33 +0545

    Functions demo in python

---
+++ functions.py
@@ -0,0 +1,17 @@
+# Functions in python are objects that can be passed around. So, we can pass one function as parameter in other function as well
+
+# function that takes arguments
+def argumentFunc(arg1, arg2):
+  pring(arg1, " ", arg2)
+  
+# function to return something
+def findCube(x)
+  return x*x*x
+  
+# Calling a function
+argumentFunc(10,20)             #10 20
+print(argumentFunc(10,20))      #10 20 and in new line none as it returns nothing
+
+pring(findCube(2))              #8
+
+  
