0aba131d97bf6d1b2e0fc5a48f8727f690dc2d90
tarabaral <73013994+tarabaral@users.noreply.github.com>
2020-10-24 23:58:31 +0545

    if elif else statement in python

---
+++ conditional.py
@@ -0,0 +1,24 @@
+# If elif else
+
+def main():
+  x,y= 10.100
+  
+  if(x < y):
+    msg="x is less than y"
+    
+  elif(x == y):
+    msg="x and y are equal"
+    
+  else:
+    msg="x is greater than y"
+    
+  print(msg)
+  
+  
+  # OUTPUT:
+  # s is less than y
+  
+  
+  # TIP
+  # unlike java, python do not show compile time error when the variable is not given a default value. 
+  
