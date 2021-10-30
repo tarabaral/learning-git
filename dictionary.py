8c770c2025c639a0644483793ef7fee54b0c11be
Tara Baral <atrabaral@gmail.com>
2021-10-30 08:54:17 +0545

    Added sample code for python dictionary

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+mydict = {
+    "fruit":"apple",
+    "year":"2021",
+    "Month":"October"
+}
+
+print(mydict["year"])
+
+# output is 2021
+# We can access dictionary's value using the key.
+# In this case, year is key and 2021 is value
